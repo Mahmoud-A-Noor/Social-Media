200c2f8a0e7e3edd524f2324c9e79f7cf8119245
mahmoudnoor <mahmoudnoor917@gmail.com>
2024-11-29 14:00:33 +0200

    Fixed issue with axios base url in axios config file

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE,
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // Interceptor for adding access token to headers
